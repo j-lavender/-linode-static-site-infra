8a6916c36ed77fd09de9150c5065b5c2fc3dbbca
lavenderguitar <jlavend1@gmail.com>
2022-02-11 17:43:21 -0500

    Explain region discrepancy

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "prod-env"
     key    = "./terraform.tfstate"
-    region = "us-east-1"                                # e.g. us-east-1
+    region = "us-east-1"                                # e.g. us-east-1  ## Region must be hardcoded to an AWS allowed region in order for this resource block to work.
     endpoint = "us-southeast-1.linodeobjects.com"       # e.g. us-east-1.linodeobjects.com
     skip_credentials_validation = true                  # Terraform will ask AWS about credential validation instead of Linode if this is enabled.
   }
